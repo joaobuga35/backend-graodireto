7636fb0cf06ef1d46fb6053607b95acb820e3448
joaobuga35 <joaobuga35@gmail.com>
2023-08-20 00:17:02 -0300

    feat: add entity user

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,1 +1,17 @@
-export class User {}
+import { Exclude } from 'class-transformer';
+
+export class User {
+  readonly id: string;
+  name: string;
+  email: string;
+
+  @Exclude()
+  password: string;
+
+  admin: boolean;
+  createdAt: string;
+
+  constructor() {
+    this.createdAt = String(new Date());
+  }
+}
