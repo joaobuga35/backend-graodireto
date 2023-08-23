9ba9c67e46779a0580845d91313addfa4fca2b9c
joaobuga35 <joaobuga35@gmail.com>
2023-08-23 15:51:12 -0300

    feat: add foods swagger ui

--- src/modules/foods/dto/update-food.dto.ts
+++ src/modules/foods/dto/update-food.dto.ts
@@ -1,4 +1,51 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateFoodDto } from './create-food.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsNumberString, IsOptional } from 'class-validator';
 
-export class UpdateFoodDto extends PartialType(CreateFoodDto) {}
+export class UpdateFoodDto extends PartialType(CreateFoodDto) {
+  @IsString()
+  @IsOptional()
+  @ApiProperty({
+    description: 'Name',
+    type: String,
+    default: 'Strognoff',
+  })
+  name: string;
+
+  @IsString()
+  @IsOptional()
+  @ApiProperty({
+    description: 'description',
+    type: String,
+    default: 'O melhor da cidade.',
+  })
+  description: string;
+
+  @IsNumberString()
+  @IsOptional()
+  @ApiProperty({
+    description: 'price',
+    type: String,
+    default: '29.90',
+  })
+  price: string;
+
+  @IsString()
+  @IsOptional()
+  @ApiProperty({
+    description: 'Image',
+    type: String,
+    default: 'www.imagem.com',
+  })
+  image: string;
+
+  @IsString()
+  @IsOptional()
+  @ApiProperty({
+    description: 'restaurantId',
+    type: String,
+    default: 'a69ee399-e0c3-4434-879a-c85da54ad762',
+  })
+  restaurantId: string;
+}
--- src/modules/foods/foods.controller.ts
+++ src/modules/foods/foods.controller.ts
@@ -13,7 +13,7 @@ import { FoodsService } from './foods.service';
 import { CreateFoodDto } from './dto/create-food.dto';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { UpdateFoodDto } from './dto/update-food.dto';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Foods')
 @Controller('foods')
@@ -22,6 +22,24 @@ export class FoodsController {
 
   @Post()
   @ApiBearerAuth()
+  @ApiOkResponse({
+    description: 'Food created successfully',
+    content: {
+      'application/json': {
+        example: {
+          createdAt:
+            'Wed Aug 23 2023 04:09:23 GMT-0300 (Horário Padrão de Brasília)',
+          id: '072299eb-befc-411e-b6f0-566065bc2dc6',
+          name: 'Arroz com filé mignon',
+          description: 'O melhor mignon',
+          image:
+            'https://1.bp.blogspot.com/-wvjKKcHCFqU/VlSCE1-eWaI/AAAAAAAACt0/ysGLpwTrsRg/s1600/1.jpg',
+          price: '79.90',
+          restaurantId: '1c555f18-d292-4d98-91c9-4e16e1d63125',
+        },
+      },
+    },
+  })
   @UseGuards(JwtAuthGuard)
   create(@Body() createFoodDto: CreateFoodDto) {
     return this.foodsService.create(createFoodDto);
@@ -30,6 +48,24 @@ export class FoodsController {
   @Get()
   @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
+  @ApiOkResponse({
+    description: 'Food created successfully',
+    content: {
+      'application/json': {
+        example: {
+          createdAt:
+            'Wed Aug 23 2023 04:09:23 GMT-0300 (Horário Padrão de Brasília)',
+          id: '072299eb-befc-411e-b6f0-566065bc2dc6',
+          name: 'Arroz com filé mignon',
+          description: 'O melhor mignon',
+          image:
+            'https://1.bp.blogspot.com/-wvjKKcHCFqU/VlSCE1-eWaI/AAAAAAAACt0/ysGLpwTrsRg/s1600/1.jpg',
+          price: '79.90',
+          restaurantId: '1c555f18-d292-4d98-91c9-4e16e1d63125',
+        },
+      },
+    },
+  })
   findAll() {
     return this.foodsService.findAll();
   }
@@ -37,6 +73,24 @@ export class FoodsController {
   @Get(':id')
   @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
+  @ApiOkResponse({
+    description: 'Food created successfully',
+    content: {
+      'application/json': {
+        example: {
+          createdAt:
+            'Wed Aug 23 2023 04:09:23 GMT-0300 (Horário Padrão de Brasília)',
+          id: '072299eb-befc-411e-b6f0-566065bc2dc6',
+          name: 'Arroz com filé mignon',
+          description: 'O melhor mignon',
+          image:
+            'https://1.bp.blogspot.com/-wvjKKcHCFqU/VlSCE1-eWaI/AAAAAAAACt0/ysGLpwTrsRg/s1600/1.jpg',
+          price: '79.90',
+          restaurantId: '1c555f18-d292-4d98-91c9-4e16e1d63125',
+        },
+      },
+    },
+  })
   findOne(@Param('id') id: string) {
     return this.foodsService.findOne(id);
   }
@@ -44,6 +98,24 @@ export class FoodsController {
   @Patch(':id')
   @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
+  @ApiOkResponse({
+    description: 'Food created successfully',
+    content: {
+      'application/json': {
+        example: {
+          createdAt:
+            'Wed Aug 23 2023 04:09:23 GMT-0300 (Horário Padrão de Brasília)',
+          id: '072299eb-befc-411e-b6f0-566065bc2dc6',
+          name: 'Arroz com filé mignon',
+          description: 'O melhor mignon',
+          image:
+            'https://1.bp.blogspot.com/-wvjKKcHCFqU/VlSCE1-eWaI/AAAAAAAACt0/ysGLpwTrsRg/s1600/1.jpg',
+          price: '79.90',
+          restaurantId: '1c555f18-d292-4d98-91c9-4e16e1d63125',
+        },
+      },
+    },
+  })
   update(@Param('id') id: string, @Body() updateFoodDto: UpdateFoodDto) {
     return this.foodsService.update(id, updateFoodDto);
   }
