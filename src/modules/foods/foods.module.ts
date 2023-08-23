4783f29a6bbc0e4c87d88bcc6e5ebec2ea507ddd
joaobuga35 <joaobuga35@gmail.com>
2023-08-23 15:00:45 -0300

    feat: add middleware on adm PATCH and DELETE method on foods route

--- src/modules/foods/foods.module.ts
+++ src/modules/foods/foods.module.ts
@@ -1,9 +1,15 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { FoodsService } from './foods.service';
 import { FoodsController } from './foods.controller';
 import { PrismaService } from 'src/database/prisma.service';
 import { FoodsRepository } from './repositories/foods.repository';
 import { FoodsPrismaRepository } from './repositories/prisma/foods.repository.prisma';
+import { AuthenticationAdm } from 'src/middlewares/authentication/authentication.adm';
 
 @Module({
   controllers: [FoodsController],
@@ -14,4 +20,13 @@ import { FoodsPrismaRepository } from './repositories/prisma/foods.repository.pr
   ],
   exports: [FoodsService],
 })
-export class FoodsModule {}
+export class FoodsModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthenticationAdm)
+      .forRoutes(
+        { path: 'foods/:id', method: RequestMethod.PATCH },
+        { path: 'foods/:id', method: RequestMethod.DELETE },
+      );
+  }
+}
